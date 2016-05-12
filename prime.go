a853e60e74a0a5106d3762b178deafdc64be1a40
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-05-12 10:51:57 -0400

    Format gofmt

--- prime.go
+++ prime.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"os"
 	"fmt"
 	"math"
+	"os"
 	"strconv"
 )
 
 func main() {
-	if (len(os.Args) == 2){
+	if len(os.Args) == 2 {
 		n, _ := strconv.ParseInt(os.Args[1], 10, 64)
 		fmt.Println(largestPrime(int(n)))
 	}
@@ -16,30 +16,30 @@ func main() {
 
 func largestPrime(n int) int {
 
-	var index_arr = make([]bool, n + 1)
+	var index_arr = make([]bool, n+1)
 	memsetRepeat(index_arr, true)
-	
+
 	var factor = 2
 	var exhaust = false
-	
+
 	for !exhaust && float64(factor) <= math.Sqrt(float64(n)) {
 		var multiple = 2
 		for factor*multiple <= n {
-			index_arr [factor*multiple] = false
-			multiple ++
+			index_arr[factor*multiple] = false
+			multiple++
 		}
-		factor ++
+		factor++
 		var next_match = indexTrue(index_arr[factor:])
-		if (next_match != -1){
+		if next_match != -1 {
 			factor += next_match
-		}else{
+		} else {
 			exhaust = true
 		}
 	}
 
 	var l_prime = indexTrueReverse(index_arr)
-	
-	if (l_prime != -1){
+
+	if l_prime != -1 {
 		return l_prime
 	}
 
@@ -48,7 +48,7 @@ func largestPrime(n int) int {
 
 func indexTrueReverse(a []bool) int {
 	for i := len(a) - 1; i >= 0; i-- {
-		if (a[i]) {
+		if a[i] {
 			return i
 		}
 	}
@@ -65,11 +65,11 @@ func indexTrue(a []bool) int {
 }
 
 func memsetRepeat(a []bool, v bool) {
-    if len(a) == 0 {
-        return
-    }
-		  a[0] = v
-    for bp := 1; bp < len(a); bp *= 2 {
-        copy(a[bp:], a[:bp])
-    }
+	if len(a) == 0 {
+		return
+	}
+	a[0] = v
+	for bp := 1; bp < len(a); bp *= 2 {
+		copy(a[bp:], a[:bp])
+	}
 }
